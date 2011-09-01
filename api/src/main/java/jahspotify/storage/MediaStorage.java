2f7e936fd43f077772c12d7c69cc15e12292fe6c
johan <johan@oden.(none)>
2011-09-01 16:45:20 -0400

    committed now?

--- api/src/main/java/jahspotify/storage/MediaStorage.java
+++ api/src/main/java/jahspotify/storage/MediaStorage.java
@@ -5,22 +5,17 @@ import jahspotify.media.*;
 /**
  * @author Johan Lindquist
  */
-public interface JahStorage
+public interface MediaStorage
 {
     public void store(Track track);
     public Track readTrack(Link uri);
     public void deleteTrack(Link uri);
-
     public void store(Artist artist);
     public Artist readArtist(Link uri);
-
     public void store(Album album);
     public Album readAlbum(Link uri);
-
     public void store(Playlist playlist);
     public Playlist readPlaylist(Link uri);
-
     public void store(Image image);
     public Image readImage(Link uri);
-
 }
