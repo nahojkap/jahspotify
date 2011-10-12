5a4c081e2211b18b87f0a573190127cf9d51d56b
Johan Lindquist <johan@loki.(none)>
2011-10-11 21:40:01 -0400

    added logo

---
+++ android/src/main/java/jahspotify/android/activities/HomeActivity.java
@@ -0,0 +1,10 @@
+package jahspotify.android.activities;
+
+import android.app.Activity;
+
+/**
+ * @author Johan Lindquist
+ */
+public class HomeActivity extends Activity
+{
+}
---
+++ android/src/main/java/jahspotify/android/activities/LauncherActivity.java
@@ -0,0 +1,10 @@
+package jahspotify.android.activities;
+
+import android.app.Activity;
+
+/**
+ * @author Johan Lindquist
+ */
+public class LauncherActivity extends Activity
+{
+}
