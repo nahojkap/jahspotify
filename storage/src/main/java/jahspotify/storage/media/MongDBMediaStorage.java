a5e05bfb33e9d4ffffdf707ae019d10b5be24611
johan <johan@oden.(none)>
2012-02-09 20:30:30 -0500

    begun mongodb storage added server broadcaster service

--- storage/src/main/java/jahspotify/storage/media/MongDBMediaStorage.java
+++ storage/src/main/java/jahspotify/storage/media/MongDBMediaStorage.java
@@ -1,8 +1,11 @@
 package jahspotify.storage.media;
 
+import java.io.FileReader;
 import javax.annotation.PostConstruct;
 
+import com.google.gson.Gson;
 import com.mongodb.*;
+import com.mongodb.util.JSON;
 import jahspotify.media.*;
 import org.apache.commons.logging.*;
 import org.springframework.beans.factory.annotation.*;
@@ -41,23 +44,49 @@ public class MongDBMediaStorage implements MediaStorage
         }
     }
 
+    public static void main(String[] args) throws Exception
+    {
+        MongDBMediaStorage mongDBMediaStorage = new MongDBMediaStorage();
+        mongDBMediaStorage.initialize();
+        Track track = new Gson().fromJson(new FileReader("/var/lib/jahspotify/simple-file-storage/tracks/3ZE8LPdngaax7rboeT7vtl"),Track.class );
+        mongDBMediaStorage.store(track);
+        Track readTrack = mongDBMediaStorage.readTrack(Link.create("spotify:track:3ZE8LPdngaax7rboeT7vtl"));
+    }
+
     @Override
     public void store(final Track track)
     {
         final DBCollection tracks = _db.getCollection("tracks");
-        final BasicDBObject basicDBObject = new BasicDBObject();
-        tracks.insert(basicDBObject);
+        final BasicDBObject basicDBObject = (BasicDBObject) JSON.parse(new Gson().toJson(track));
+        basicDBObject.put("_id",track.getId().getId());
+        final WriteResult insert = tracks.insert(basicDBObject);
     }
 
     @Override
     public Track readTrack(final Link uri)
     {
+        final DBCollection tracks = _db.getCollection("tracks");
+
+        BasicDBObject query = new BasicDBObject();
+        query.put("_id", uri.getId());
+
+        final DBObject one = tracks.findOne(query);
+        
+        if (one != null)
+        {
+            return new Gson().fromJson(JSON.serialize(one),Track.class);
+        }
+
         return null;
     }
 
     @Override
     public void deleteTrack(final Link uri)
     {
+        final DBCollection tracks = _db.getCollection("tracks");
+        BasicDBObject query = new BasicDBObject();
+        query.put("_id", uri.getId());
+        final DBObject andRemove = tracks.findAndRemove(query);
     }
 
     @Override
