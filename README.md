84507a9314369dc4013c793a873595840f47858e
Niels van de Weem <NielsGithub@vdweem.com>
2012-06-15 19:28:46 +0200

    OpenAL does work on Windows :)
    Added documentation as to where one can find it and put the code using
    it back.

--- README.md
+++ README.md
@@ -7,15 +7,22 @@ An attempt to get the Jah'Spotify project compatible with Windows. For the origi
 ## Before compiling
 
 - I did not test this on Linux. If you use Linux then use the Johan Lindquist branch. It should still work though.
-- The native pom.xml has a reference to my local LibSpotify folder. Change this to your own Spotify folder. You'll get an error that the api.h file can't be found if you don't.
 - Download MinGW and put the bin folder in your PATH.
+- Download the OpenAL SDK from and copy the include folder to the MinGW include folder and rename it to AL
+- The native pom.xml has a reference to my local LibSpotify folder. Change this to your own Spotify folder. You'll get an error that the api.h file can't be found if you don't.
+- Also add the reference to your OpenAL installation folder
 - For the rest, use Johan's steps for building.
+
+You can find the OpenAL SDK at:
+
+    http://connect.creativelabs.com/developer/Wiki/OpenAL%20SDK%20for%20Windows.aspx
 
 ## Running
 The tool isn't quite working yet. Following the compile steps you will be able to run the Java part, but it won't o anything useful yet.
 You will also need to download a few more dependencies:
 - phtread (http://sources.redhat.com/pthreads-win32/). pthreadGC2.dll needs to be in your path.
 - mongodb, and have the daemon running.
+- I didn't have to add any paths to OpenAL but I installed the SDK. Let me know if you get a message complaining that not all dependencies are available.
 
 Johan states that the run command is as follows:
 
--- native/src/main/native/src/openal-audio.c
+++ native/src/main/native/src/openal-audio.c
@@ -25,6 +25,8 @@
  * This file is part of the libspotify examples suite.
  */
 
+#include <AL/al.h>
+#include <AL/alc.h>
 #include <errno.h>
 #include <math.h>
 #include <stdio.h>
@@ -34,6 +36,11 @@
 
 #include "audio.h"
 #include "Logging.h"
+
+#define NUM_BUFFERS 3
+
+static ALuint source = 0;
+
 
 static void error_exit(const char *msg)
 {
@@ -46,20 +53,44 @@ static void error_exit(const char *msg)
 void audio_close()
 {
     
+    ALCdevice *device = NULL;
+    ALCcontext *context = NULL;
+    // Exit
+    context=alcGetCurrentContext();
+    if (context)
+    {
+        device=alcGetContextsDevice(context);
+        alcMakeContextCurrent(NULL);
+        alcDestroyContext(context);
+        if (device)
+            alcCloseDevice(device);
+    }
+
 }
 
 float get_audio_gain()
 {
-    return -1;
+    ALfloat currentGain = -1;
+    alGetSourcef(source, AL_GAIN, &currentGain);
+    return currentGain;
 }
 
 void set_audio_gain(float gain)
 {
+    alSourcef(source, AL_GAIN, gain);
 }
 
 
-static int queue_buffer(void* source, audio_fifo_t *af, void* buffer)
+static int queue_buffer(ALuint source, audio_fifo_t *af, ALuint buffer)
 {
+    audio_fifo_data_t *afd = audio_get(af);
+    alBufferData(buffer,
+                 afd->channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16,
+                 afd->samples,
+                 afd->nsamples * afd->channels * sizeof(short),
+                 afd->rate);
+    alSourceQueueBuffers(source, 1, &buffer);
+    free(afd);
     return 1;
 }
 
@@ -67,17 +98,141 @@ static int queue_buffer(void* source, audio_fifo_t *af, void* buffer)
 
 static void* audio_start(void *aux)
 {
+    audio_fifo_t *af = aux;
+    audio_fifo_data_t *afd;
+    ALCdevice *device = NULL;
+    ALCcontext *context = NULL;
+    ALuint buffers[NUM_BUFFERS];
+    ALint processed;
+    ALenum error;
+    ALint rate;
+    ALint channels;
     
+    device = alcOpenDevice(NULL); /* Use the default device */
+    if (!device) error_exit("failed to open device");
+    context = alcCreateContext(device, NULL);
+    alcMakeContextCurrent(context);
+    alListenerf(AL_GAIN, 1.0f);
+    alDistanceModel(AL_NONE);
+    alGenBuffers((ALsizei)NUM_BUFFERS, buffers);
+    alGenSources(1, &source);
+
+    /* First prebuffer some audio */
+    queue_buffer(source, af, buffers[0]);
+    queue_buffer(source, af, buffers[1]);
+    queue_buffer(source, af, buffers[2]);
+    for (;;) {
+
+        alSourcePlay(source);
+        for (;;) {
+            /* Wait for some audio to play */
+            alGetSourcei(source, AL_BUFFERS_PROCESSED, &processed);
+            if (processed <= 0)
+            {
+                usleep(200);
+                continue;
+            }
+
+            /* Remove old audio from the queue.. */
+            ALuint buffer;
+            
+            alSourceUnqueueBuffers(source, 1, &buffer);
+
+            /* and queue some more audio */
+            afd = audio_get(af);
+
+            alGetBufferi(buffer, AL_FREQUENCY, &rate);
+            alGetBufferi(buffer, AL_CHANNELS, &channels);
+            if (afd->rate != rate || afd->channels != channels) 
+            {
+                log_debug("openal","audio_start","rate or channel count changed, resetting");
+                break;
+            }
+
+            alBufferData(buffer,
+                afd->channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16,
+                afd->samples,
+                afd->nsamples * afd->channels * sizeof(int16_t),
+                afd->rate);
+
+            free(afd);
+
+            ALenum error = alGetError();
+            if (error != AL_NO_ERROR)
+            {
+                log_error("openal","audio_start","Error buffering: %s", alGetString(error));
+                return;
+            }
+
+            alSourceQueueBuffers(source, 1, &buffer);
+
+            error = alGetError();
+            if (alGetError() != AL_NO_ERROR)
+            {
+                log_error("openal","audio_start","Error queing buffering: %s", alGetString(error));
+                return;
+            }
+
+
+            alGetSourcei(source, AL_SOURCE_STATE, &processed);
+            if (processed != AL_PLAYING)
+            {
+                // Resume playing
+                alSourcePlay(source);
+            }
+
+            if ((error = alcGetError(device)) != AL_NO_ERROR) {
+                log_error("openal","audio_start","Error queing buffering: %s", alGetString(error));
+                exit(1);
+            }
+            
+        }
+
+        /* Format or rate changed, so we need to reset all buffers */
+        alSourcei(source, AL_BUFFER, 0);
+        alSourceStop(source);
+
+        /* Make sure we don't lose the audio packet that caused the change */
+        alBufferData(buffers[0],
+                     afd->channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16,
+                     afd->samples,
+                     afd->nsamples * afd->channels * sizeof(short),
+                     afd->rate);
+
+        free(afd);
+
+        alSourceQueueBuffers(source, 1, &buffers[0]);
+        queue_buffer(source, af, buffers[1]);
+        queue_buffer(source, af, buffers[2]);
+      
+    }
 }
 
 
 void audio_init(audio_fifo_t *af)
 {
+    pthread_t tid;
 
+    TAILQ_INIT(&af->q);
+    af->qlen = 0;
+
+    pthread_mutex_init(&af->mutex, NULL);
+    pthread_cond_init(&af->cond, NULL);
+
+    pthread_create(&tid, NULL, audio_start, af);
 }
 
 void audio_fifo_flush(audio_fifo_t *af)
 {
-   
-}
+    audio_fifo_data_t *afd;
 
+    pthread_mutex_lock(&af->mutex);
+
+    while ((afd = TAILQ_FIRST(&af->q))) {
+        TAILQ_REMOVE(&af->q, afd, link);
+        free(afd);
+    }
+
+    af->qlen = 0;
+    pthread_mutex_unlock(&af->mutex);
+}
